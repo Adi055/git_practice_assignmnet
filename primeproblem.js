c3a0ad91d899ad8a0dfd4ab3298f05540f63bd63
Aditya tiwari <at257645@gmail.com>
2023-01-03 22:38:48 +0530

    prime and palindrome

---
+++ primeproblem.js
@@ -0,0 +1,28 @@
+a=13;
+c=0
+for(let i=1; i<=a; i++){
+if(a%i==0){
+c++;
+}
+}
+if(c==1){
+console.log("prime")
+}
+else{
+("Not Prime")
+}
+
+
+
+\\
+a="level"
+c=0;
+for(let i=a.length-1; i>=0; i--){
+c+=a[i];
+}
+if(c==a){
+console.log("Yes")
+)
+else{
+('No')
+}
